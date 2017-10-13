dccfd9a519c363896def26b4f7949cdafdd3976c
palyon <patrick@Patricks-MacBook-Pro.local>
2017-10-12 21:20:41 -0400

    Figured out how to select particular results from the API, added and skeleton to the ReadMe, and added some loose html

---
+++ index.js
@@ -0,0 +1,33 @@
+$(function(){
+	$("form").submit(function(e){
+		e.preventDefault();
+
+		var songSearch = $(".title-search").val();
+
+		$.ajax({
+		    type: "GET",
+		    data: {
+		        apikey:"bea1ecda96ff636cf5c843126a47a727",
+		        q_track: songSearch,
+		        f_has_lyrics: 1,
+		        format:"jsonp",
+		        callback:"jsonp_callback"
+		    },
+		    url: "http://api.musixmatch.com/ws/1.1/track.search",
+		    dataType: "jsonp",
+		    jsonpCallback: 'jsonp_callback',
+		    contentType: 'application/json',
+		    success: function(data) {
+						var test = data.message.body.track_list;
+						test.forEach(function(track_list){
+							console.log(track_list.track.track_id)
+						})
+		    },
+		    error: function(jqXHR, textStatus, errorThrown) {
+		        console.log(jqXHR);
+		        console.log(textStatus);
+		        console.log(errorThrown);
+		    }
+		 });
+	})
+})
