e14cbd702ab627290689c1675cb01627c2547a16
Juan Henao <juandi310@gmail.com>
2017-10-12 20:20:49 -0400

    succesfully used js to obtain get command from API

--- script.js
+++ script.js
@@ -2,15 +2,31 @@ $(function(){
 	$("form").submit(function(e){
 		e.preventDefault();
 
-		var nameSearch = $(".title-search").val();
-		console.log(nameSearch);
+		var songSearch = $(".title-search").val();
 
-
-		 // var url = "http://api.musixmatch.com/ws/1.1/track.search&apikey=bea1ecda96ff636cf5c843126a47a727&q_track=" + nameSearch;
-
-		 // $.get(url, function(title){
-
-		})
+		$.ajax({
+		    type: "GET",
+		    data: {
+		        apikey:"bea1ecda96ff636cf5c843126a47a727",
+		        q_track: songSearch,
+		        f_has_lyrics: 1,
+		        format:"jsonp",
+		        callback:"jsonp_callback"
+		    },
+		    url: "http://api.musixmatch.com/ws/1.1/track.search",
+		    dataType: "jsonp",
+		    jsonpCallback: 'jsonp_callback',
+		    contentType: 'application/json',
+		    success: function(data) {
+		        console.log(data); 
+		    },
+		    error: function(jqXHR, textStatus, errorThrown) {
+		        console.log(jqXHR);
+		        console.log(textStatus);
+		        console.log(errorThrown);
+		    }    
+		 });
 
 	})
-// })
+})
+
