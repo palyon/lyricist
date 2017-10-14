6fe514dcb393a287b7f1589f171c3ba83774e1ac
palyon <patrick@Patricks-MacBook-Pro.local>
2017-10-14 14:08:32 -0400

    Added a click listener to the View Lyrics Buttons and console.logs the results from the API with the lyrics

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# lyricist
-DigitalCrafts Group Project
-Test Commit
-LyrAssist
+# LyrAssist
+
+Overview of the Project
+LyrAssist is a web-based application that allows you to find the lyrics of a song and maybe even discover some new music along the way.
+
+Users enter in either a Song Name or any word and LyrAssist runs a query to the MusixMatch API and spits back 10 results - all with the Track Name containing the queried word. The results display the Track Name and Artist Name in the form of Html-Cards. When clicked, LyrAssist sends another query to the MusixMatch API, using the Track ID, to retrieve and display 30% of the song lyrics to the user.
+
+Contributors
+[Charlie Lynch]
+[Juan Henao]
+[Patrick Lyons]
+
+Technologies, Frameworks, and Programming Languages Used
+HTML
+Bootstrap
+JavaScript
+JQuery
+MusixMatch API
+**[Graphic API]**
+Project Screenshots
+[Content Here]
+
+Link to the Project
+[Content Here]
--- index.js
+++ index.js
@@ -1,8 +1,68 @@
 $(function(){
-	$("form").submit(function(e){
+
+	function renderSongs(songs) {
+
+		$(".card-columns").empty();
+
+		songs.forEach(function(song){
+			// console.log(song.track.track_name);
+
+			var newDiv = $('<div class="card"></div>');
+			var cardImg = $('<img class="card-img-top" src="img/song-pic.jpg">');
+			var newCardBody = $('<div class="card-body"></div>');
+			var newSongTitle = $('<h4 class="card-title"></h4>');
+			var newArtist = $('<p class="card-text"></p>');
+			var newButton = $('<button type="button" class="btn btn-info btn-sm get-lyrics"> View Lyrics</button>');
+
+			newSongTitle.text(song.track.track_name);
+			newArtist.text(song.track.artist_name);
+			newButton.val(song.track.track_id);
+			console.log(newButton.val())
+
+			newCardBody.append(newSongTitle, newArtist, newButton);
+			newDiv.append(cardImg,newCardBody);
+
+			$(".card-columns").append(newDiv);
+
+		 });
+
+		$('.get-lyrics').on('click', function(e){
+			e.preventDefault();
+
+			var buttonValue = $(".get-lyrics").val();
+
+			$.ajax({
+			    type: "GET",
+			    data: {
+			        apikey:"bea1ecda96ff636cf5c843126a47a727",
+			        track_id: buttonValue,
+			        f_has_lyrics: 1,
+			        format:"jsonp",
+			        callback:"jsonp_callback"
+			    },
+			    url: "http://api.musixmatch.com/ws/1.1/track.lyrics.get",
+			    dataType: "jsonp",
+			    jsonpCallback: 'jsonp_callback',
+			    contentType: 'application/json',
+			    success: function(data) {
+			        console.log(data);
+
+			    },
+			    error: function(jqXHR, textStatus, errorThrown) {
+			        console.log(jqXHR);
+			        console.log(textStatus);
+			        console.log(errorThrown);
+			    }
+			 });
+			})
+	}
+
+
+	// listener for search function
+	$("#search-form").submit(function(e){
 		e.preventDefault();
 
-		var songSearch = $(".title-search").val();
+		var songSearch = $(".search-bar").val();
 
 		$.ajax({
 		    type: "GET",
@@ -18,16 +78,27 @@ $(function(){
 		    jsonpCallback: 'jsonp_callback',
 		    contentType: 'application/json',
 		    success: function(data) {
-						var test = data.message.body.track_list;
-						test.forEach(function(track_list){
-							console.log(track_list.track.track_id)
-						})
+		        console.log(data);
+
+		        var results = data.message.body.track_list;
+		        renderSongs(results);
+		        	// results.forEach(function(track_list){
+		        	// 	var artist = track_list.track.artist_name;
+		        	// 	var song = track_list.track.track_name;
+		        	// 	var trackId = track_list.track.track_id;
+		        	// 	// console.log(artist, song, trackId);
+		        	// })
+
+
+
 		    },
 		    error: function(jqXHR, textStatus, errorThrown) {
 		        console.log(jqXHR);
 		        console.log(textStatus);
 		        console.log(errorThrown);
 		    }
+
 		 });
+
 	})
 })
