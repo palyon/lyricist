bcb3eb9d6ed9c52af2d0e8873c088a67c146eecc
lynchlcl71 <lynchlcl@aol.com>
2017-10-15 20:56:08 -0400

    updated code for granim gradient to work effectively

--- script.js
+++ script.js
@@ -1,62 +1,119 @@
 $(function(){
-  $("#search-form").submit(function(e){
-    e.preventDefault();
 
-    var songSearch = $(".search-bar").val();
+    function renderSongs(songs) {
 
-    $.ajax({
-        type: "GET",
-        data: {
-            apikey:"bea1ecda96ff636cf5c843126a47a727",
-            q_track: songSearch,
-            f_has_lyrics: 1,
-            format:"jsonp",
-            callback:"jsonp_callback"
-        },
-        url: "http://api.musixmatch.com/ws/1.1/track.search",
-        dataType: "jsonp",
-        jsonpCallback: 'jsonp_callback',
-        contentType: 'application/json',
-        success: function(data) {
-            console.log(data); 
-            
-            var results = data.message.body.track_list;
-              results.forEach(function(track_list){
-                var artist = track_list.track.artist_name;
-                var song = track_list.track.track_name;
-                var trackId = track_list.track.track_id;
-                console.log(artist, song, trackId);
-              })
+        $(".card-columns").empty();
+
+        songs.forEach(function(song){
+
+            // generate HTML and dipslay new cards on the screen
+
+            var newDiv = $('<div class="card"></div>');
+            var cardImg = $('<img class="card-img-top" src="img/song-pic.jpg">');
+            var newCardBody = $('<div class="card-body"></div>');
+            var newSongTitle = $('<h4 class="card-title"></h4>');
+            var newArtist = $('<p class="card-text"></p>');
+            var newButton = $('<button type="button" class="btn btn-info btn-sm get-lyrics"> View Lyrics</button>');
+
+            newSongTitle.text(song.track.track_name);
+            newArtist.text(song.track.artist_name);
+            newButton.val(song.track.track_id);
+
+            newCardBody.append(newSongTitle, newArtist, newButton);
+            newDiv.append(cardImg,newCardBody);
+
+            $(".card-columns").append(newDiv);
+
+        })
 
 
-           
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log(jqXHR);
-            console.log(textStatus);
-            console.log(errorThrown);
-        }  
+        $(".get-lyrics").on("click", function(e){
+            e.preventDefault();
 
-     });
+            var buttonValue = $(".get-lyrics").val();
 
-  })
-var granimInstance = new Granim({
-    element: '#canvas-basic',
-    name: 'basic-gradient',
-    direction: 'left-right',
-    opacity: [1, 1],
-    isPausedWhenNotInView: true,
-    states : {
-        "default-state": {
-            gradients: [
-                ['#AA076B', '#61045F'],
-                ['#02AAB0', '#00CDAC'],
-                ['#DA22FF', '#9733EE']
-            ]
-            
-        }
+            $.ajax({
+                type: "GET",
+                data: {
+                    apikey:"bea1ecda96ff636cf5c843126a47a727",
+                    track_id: buttonValue,
+                    f_has_lyrics: 1,
+                    format:"jsonp",
+                    callback:"jsonp_callback"
+                },
+                url: "http://api.musixmatch.com/ws/1.1/track.lyrics.get",
+                dataType: "jsonp",
+                jsonpCallback: 'jsonp_callback',
+                contentType: 'application/json',
+                success: function(data) {
+                    console.log(data);
+
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR);
+                    console.log(textStatus);
+                    console.log(errorThrown);
+                }
+             });
+            })
     }
-});
-})
 
+    // listener for search function
+
+    $("#search-form").submit(function(e){
+        e.preventDefault();
+
+        var songSearch = $(".search-bar").val();
+
+        $.ajax({
+            type: "GET",
+            data: {
+                apikey:"bea1ecda96ff636cf5c843126a47a727",
+                q_track: songSearch,
+                f_has_lyrics: 1,
+                format:"jsonp",
+                callback:"jsonp_callback"
+            },
+            url: "http://api.musixmatch.com/ws/1.1/track.search",
+            dataType: "jsonp",
+            jsonpCallback: 'jsonp_callback',
+            contentType: 'application/json',
+            success: function(data) {
+                console.log(data); 
+                
+                var results = data.message.body.track_list;
+                renderSongs(results);
+                    // results.forEach(function(track_list){
+                    //  var artist = track_list.track.artist_name;
+                    //  var song = track_list.track.track_name;
+                    //  var trackId = track_list.track.track_id;
+                    //  // console.log(artist, song, trackId);
+                    // })
+               
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+            }  
+
+         });
+
+    })
+        var granimInstance = new Granim({
+            element: '#canvas-image',
+            direction: 'top-bottom',
+            opacity: [1, .5, 0],
+            isPausedWhenNotInView: true,
+            states : {
+                "default-state": {
+                    gradients: [
+                        ['#485563', '#29323c', '#29323c'],
+                        ['#00c6ff', '#0072ff', '#0072ff']
+                    ],
+                    transitionSpeed: 6000
+                }
+            }
+        });
+    })
 
