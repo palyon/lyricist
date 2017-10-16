36d3ceb50d1d358496cb245294d429bd314633a2
Juan Henao <juandi310@gmail.com>
2017-10-16 00:09:59 -0400

    implemented granim API, made entire site responsive and added hover effect to lyrics button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(function(){
 
+
 	function renderSongs(songs) {
 
 		$(".card-columns").empty();
@@ -13,49 +14,66 @@ $(function(){
 			var newCardBody = $('<div class="card-body"></div>');
 			var newSongTitle = $('<h4 class="card-title"></h4>');
 			var newArtist = $('<p class="card-text"></p>');
-			var newButton = $('<button type="button" class="btn btn-info btn-sm get-lyrics"> View Lyrics</button>');
+			var newButton = $('<button type="button" class="btn btn-info btn-sm get-lyrics" data-toggle="modal" data-target=".lyricContent"> View Lyrics</button>');
+			var newModalContent =$('<div class="modal fade lyricContent" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">' +
+				'<div class="modal-dialog modal-lg">' +
+					'<div class="modal-content">' +
+							'<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+								'<span aria-hidden="true">&times;</span>' +
+							'</button>' +
+						'<div class="modal-body">' +
+							'<p class="songLyricsHere"></p>' +
+						'</div>' +
+					'</div>' +
+				'</div>' +
+			'</div>');
+
 
 			newSongTitle.text(song.track.track_name);
 			newArtist.text(song.track.artist_name);
 			newButton.val(song.track.track_id);
+			console.log(newButton.val());
 
-			newCardBody.append(newSongTitle, newArtist, newButton);
+			newCardBody.append(newSongTitle, newArtist, newButton, newModalContent);
 			newDiv.append(cardImg,newCardBody);
 
 			$(".card-columns").append(newDiv);
 
+		});
+
+
+	$(".get-lyrics").on("click", function(e){
+		e.preventDefault();
+
+		var buttonValue = $(".get-lyrics").val();
+
+		$.ajax({
+		    type: "GET",
+		    data: {
+		        apikey:"bea1ecda96ff636cf5c843126a47a727",
+		        track_id: buttonValue,
+		        f_has_lyrics: 1,
+		        format:"jsonp",
+		        callback:"jsonp_callback"
+		    },
+		    url: "http://api.musixmatch.com/ws/1.1/track.lyrics.get",
+		    dataType: "jsonp",
+		    jsonpCallback: 'jsonp_callback',
+		    contentType: 'application/json',
+		    success: function(data) {
+		        console.log(data);
+		        		var results = data.message.body.lyrics.lyrics_body;
+		        		console.log(results);
+		        		$(".songLyricsHere").append(results);
+
+		    },
+		    error: function(jqXHR, textStatus, errorThrown) {
+		        console.log(jqXHR);
+		        console.log(textStatus);
+		        console.log(errorThrown);
+		    }
+		 });
 		})
-
-
-		$(".get-lyrics").on("click", function(e){
-			e.preventDefault();
-
-			var buttonValue = $(".get-lyrics").val();
-
-			$.ajax({
-			    type: "GET",
-			    data: {
-			        apikey:"bea1ecda96ff636cf5c843126a47a727",
-			        track_id: buttonValue,
-			        f_has_lyrics: 1,
-			        format:"jsonp",
-			        callback:"jsonp_callback"
-			    },
-			    url: "http://api.musixmatch.com/ws/1.1/track.lyrics.get",
-			    dataType: "jsonp",
-			    jsonpCallback: 'jsonp_callback',
-			    contentType: 'application/json',
-			    success: function(data) {
-			        console.log(data);
-
-			    },
-			    error: function(jqXHR, textStatus, errorThrown) {
-			        console.log(jqXHR);
-			        console.log(textStatus);
-			        console.log(errorThrown);
-			    }
-			 });
-			})
 	}
 
 	// listener for search function
@@ -99,6 +117,23 @@ $(function(){
 
 		 });
 
+	})
+
+
+	var granimInstance = new Granim({
+    element: '#canvas-image',
+    direction: 'top-bottom',
+    opacity: [1, .5, 0],
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#485563', '#29323c', '#29323c'],
+                ['#00c6ff', '#0072ff', '#0072ff']
+            ],
+            transitionSpeed: 6000
+        }
+    }
 	})
 })
 
